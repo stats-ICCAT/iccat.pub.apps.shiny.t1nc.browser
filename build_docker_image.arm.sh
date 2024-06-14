ca6d5e98c4f9b50575ec9e9fdb5c38835410a8db
Fabio Fiorellato <flowerside@gmail.com>
2024-06-14 17:48:00 +0200

    Updating dockerfile and scripts

--- build_docker_image.arm.sh
+++ build_docker_image.arm.sh
@@ -1,4 +1,4 @@
-docker build -t iccat/apps/ce_browser:0.1.0 \
+docker build -t iccat/apps/nc_browser:0.1.0 \
              --build-arg GITLAB_AUTH_TOKEN=$GITLAB_AUTH_TOKEN \
              --progress=plain \
              -f Dockerfile.arm .
