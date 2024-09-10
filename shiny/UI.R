a72fc34b1588a73336866b41b5cb210fee40bd1a
Fabio Fiorellato <flowerside@gmail.com>
2024-09-10 11:48:03 +0200

    Changing app name

--- shiny/UI.R
+++ shiny/UI.R
@@ -1,5 +1,5 @@
 ui = function() {
-  TITLE = paste0("ICCAT data browser / T1NC / ", META$LAST_UPDATE)
+  TITLE = paste0("ICCAT / Task 1 / NC / browser / ", META$LAST_UPDATE)
   return(
     fluidPage(
       shinyjs::useShinyjs(),
