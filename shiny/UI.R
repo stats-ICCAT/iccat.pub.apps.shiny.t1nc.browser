8372a1a6b3459831463d9fe436200df7f891c0c4
Fabio Fiorellato <fabio@fiorella.to>
2024-10-10 09:08:33 +0200

    Removing date from header

--- shiny/UI.R
+++ shiny/UI.R
@@ -1,5 +1,5 @@
 ui = function() {
-  TITLE = paste0("ICCAT / Task 1 / NC / browser / ", META$LAST_UPDATE)
+  TITLE = "ICCAT / Task 1 / NC / browser"
   return(
     fluidPage(
       shinyjs::useShinyjs(),
